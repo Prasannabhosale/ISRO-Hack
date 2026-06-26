0410064451595d4a95bee0722d830f7022eb49fd
Prasannabhosale <prasannavbhosale@gmail.com>
2026-06-26 13:37:28 +0530

    Implemented Ml pipeline and data preprocessing

---
+++ src/components/data_transformation.py
@@ -0,0 +1,151 @@
+import os
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from components import data_ingestion
+from components import data_ingestion
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join(
+        "artifacts",
+        "preprocessor.pkl"
+    )
+
+
+class DataTransformation:
+
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+
+        try:
+
+            numerical_columns = [
+                "AirTemp",
+                "BuiltSurface",
+                "Elevation",
+                "NDBI",
+                "NDVI",
+                "PopulationDensity",
+                "WaterDistance",
+                "WindSpeed",
+                "Longitude",
+                "Latitude"
+            ]
+
+            categorical_columns = [
+                "LULC",
+                "UrbanClass"
+            ]
+
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            cat_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("onehot", OneHotEncoder(handle_unknown="ignore"))
+                ]
+            )
+
+            logging.info(f"Numerical Columns : {numerical_columns}")
+            logging.info(f"Categorical Columns : {categorical_columns}")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipeline", cat_pipeline, categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
+
+        try:
+
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Train and Test data loaded")
+
+            preprocessing_obj = self.get_data_transformer_object()
+
+            target_column_name = "LST"
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info("Applying preprocessing")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(
+                input_feature_train_df
+            )
+
+            input_feature_test_arr = preprocessing_obj.transform(
+                input_feature_test_df
+            )
+
+            train_arr = np.c_[
+                input_feature_train_arr,
+                np.array(target_feature_train_df)
+            ]
+
+            test_arr = np.c_[
+                input_feature_test_arr,
+                np.array(target_feature_test_df)
+            ]
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+
+            logging.info("Preprocessor object saved")
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+from src.components.data_transformation import DataTransformation
+
+if __name__ == "__main__":
+    obj = data_ingestion()
+
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(
+        train_path,
+        test_path
+    )
